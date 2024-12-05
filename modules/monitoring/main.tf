f75dbdd1b81d15d41499f9d9f79c2f1428da7013
Nitzan Schwartz <nitzan.s@outlook.com>
2024-12-05 14:08:18 +0200

    added alert

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -9,3 +9,38 @@ resource "google_monitoring_notification_channel" "email" {
     email_address = var.alert_email_address
   }
 }
+
+# Alert Policy for CPU Usage
+resource "google_monitoring_alert_policy" "cpu_usage" {
+  display_name = "${var.prefix}-high-cpu-usage-policy"
+  project      = var.project_id
+  combiner     = "OR"
+
+  conditions {
+    display_name = "CPU usage above threshold"
+
+    condition_threshold {
+      filter = "resource.type = \"k8s_container\" AND resource.labels.namespace_name = \"${var.k8s_namespace}\" AND metric.type = \"kubernetes.io/container/cpu/core_usage_time\""
+      
+      duration        = "60s"
+      comparison     = "COMPARISON_GT"
+      threshold_value = var.hpa_cpu_alert_threshold
+
+      trigger {
+        count = 1
+      }
+
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_RATE"
+      }
+    }
+  }
+
+  notification_channels = [google_monitoring_notification_channel.email.id]
+
+  alert_strategy {
+    auto_close = "1800s"
+  }
+}
+
